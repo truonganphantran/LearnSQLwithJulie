25671e1537bdc7dc7bb38ebdf7870073e38f42bd
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-13 17:23:17 +0700

    Update Practice2(buoi5).sql

--- Practice2(buoi5).sql
+++ Practice2(buoi5).sql
@@ -12,3 +12,13 @@ FROM STATION;
 SELECT
 ROUND (cast (sum (item_count*order_occurrences)/sum (order_occurrences) as decimal),1) AS total
 FROM items_per_order;
+
+--bai 5
+SELECT candidate_id 
+FROM candidates
+GROUP BY candidate_id
+having COUNT(CASE WHEN skill IN ( 'Python', 'Tableau', 'PostgreSQL') then skill
+END ) = 3
+
+
+--bai 6
