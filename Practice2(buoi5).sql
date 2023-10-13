f5cda11df89d8beb310b0c6983598a46ad3a5b7c
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-13 16:10:01 +0700

    Update Practice2(buoi5).sql

--- Practice2(buoi5).sql
+++ Practice2(buoi5).sql
@@ -3,3 +3,12 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE MOD(ID,2) = 0;
 
+--bai 2
+SELECT 
+COUNT(CITY) - COUNT(DISTINCT CITY) 
+FROM STATION;
+
+--bai4
+SELECT
+ROUND (cast (sum (item_count*order_occurrences)/sum (order_occurrences) as decimal),1) AS total
+FROM items_per_order;
