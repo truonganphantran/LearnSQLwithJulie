19170cec678dceddb7faba5bec3b7c41cfb8d9ea
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-21 14:15:08 +0700

    Update Practice5(buoi11).sql

--- Practice5(buoi11).sql
+++ Practice5(buoi11).sql
@@ -8,6 +8,27 @@ INNER JOIN texts
 ON emails.email_id = texts.email_id
 
 --bai 3
+SELECT
+age_bucket
+   ,ROUND(SUM(CASE when activity_type = 'open' THEN time_spent END)*100/ SUM(CASE when activity_type IN ( 'send','open') THEN time_spent END),2 )AS open_perc
+  ,ROUND (SUM(CASE when activity_type = 'send' THEN time_spent END)*100/SUM(CASE when activity_type IN ( 'send','open') THEN time_spent END),2) AS send_perc
+FROM age_breakdown
+INNER JOIN activities
+ON age_breakdown.user_id = activities.user_id
+GROUP BY age_bucket;
+
+--bài 4
+SELECT pro.product_id
+, product_category
+, product_name
+ FROM customer_contracts cus
+RIGHT JOIN products pro
+on pro.product_id = cus.product_id
+GROUP BY pro.product_id, product_category
+, product_name
+ORDER BY pro.product_id;
+
+-bai 5
 
 
 
