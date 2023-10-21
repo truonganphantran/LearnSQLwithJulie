7736e2d88cf440d400064bc5d9baa76417ef79cf
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-21 11:46:00 +0700

    Update Practice5(buoi11).sql

--- Practice5(buoi11).sql
+++ Practice5(buoi11).sql
@@ -2,9 +2,12 @@
 
 --bai 2
 SELECT 
-CAST (count(distinct emails.email_id) AS DECIMAL)/ (SELECT COUNT(distinct email_id) FROM emails )
+CAST (count(distinct emails.email_id) AS DECIMAL)/ (SELECT COUNT(distinct email_id) FROM emails ) AS confirm_rate
 FROM emails
 INNER JOIN texts
 ON emails.email_id = texts.email_id
+
+--bai 3
+
 
 
