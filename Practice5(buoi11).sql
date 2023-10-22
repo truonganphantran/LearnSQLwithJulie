d86c26c2c7c0a3241a81cdd7a34e4b8fb7e4aba9
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-22 15:11:50 +0700

    Update Practice5(buoi11).sql

--- Practice5(buoi11).sql
+++ Practice5(buoi11).sql
@@ -1,5 +1,11 @@
 --bai 1
-
+SELECT
+    COUNTRY.CONTINENT
+    ,FLOOR(AVG(CITY.POPULATION))
+FROM CITY
+JOIN COUNTRY
+ON COUNTRY.CODE = CITY.COUNTRYCODE
+GROUP BY COUNTRY.CONTINENT;
 --bai 2
 SELECT 
 CAST (count(distinct emails.email_id) AS DECIMAL)/ (SELECT COUNT(distinct email_id) FROM emails ) AS confirm_rate
