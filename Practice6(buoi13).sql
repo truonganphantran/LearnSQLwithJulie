bd4d31abd57269a2351f70e8f297acd600cc1737
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-24 23:15:57 +0700

    Update Practice6(buoi13).sql

--- Practice6(buoi13).sql
+++ Practice6(buoi13).sql
@@ -19,4 +19,13 @@ FROM CTE1
 GROUP BY month
 
 
---
+--bài 6
+SELECT 
+EXTRACT(YEAR_MONTH FROM trans_date) AS month --khúc này em không biết làm sao để nó có dấu '-' ở giữa nữa chị
+, country
+, COUNT(id) AS trans_count
+, COUNT(CASE WHEN state = 'approved' THEN id END) AS approved_count
+, SUM(amount) AS total_amount
+, SUM(CASE WHEN state = 'approved' THEN amount END) AS approve_total_amount
+FROM Transactions
+GROUP BY EXTRACT(YEAR_MONTH FROM trans_date) , country
