1da17c8dce25d5eade1c424a0e9360a6aadfb988
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-28 12:41:51 +0700

    Update Practice7(buoi15).sql

--- Practice7(buoi15).sql
+++ Practice7(buoi15).sql
@@ -8,7 +8,24 @@ SELECT
 FROM user_transactions;
 
 --BÀI 2
-SELECT
+SELECT *
+FROM
+(SELECT
 card_name,
 FIRST_VALUE(issued_amount) OVER(PARTITION BY card_name ORDER BY issue_year, issue_month) AS amount
-FROM monthly_cards_issued
+FROM monthly_cards_issued) AS table1
+GROUP BY card_name, amount
+
+--bài 3
+SELECT user_id
+  ,spend 
+  , transaction_date
+FROM  (SELECT 
+  user_id
+  ,spend
+  ,ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) as thutu
+  , transaction_date
+FROM transactions) AS subtable
+WHERE thutu = '3';
+
+--
