c5351c0bcabd0c32d85c1ba1de19b22beb9c2f42
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-29 19:18:38 +0700

    Update practice8(buoi16).sql

--- practice8(buoi16).sql
+++ practice8(buoi16).sql
@@ -18,3 +18,40 @@ FROM (SELECT player_id
 FROM activity) AS subtable
 
 --BÀI 3
+
+SELECT id
+    ,CASE WHEN MOD(id,2)=1 AND col1 IS NOT NULL THEN col1 
+          WHEN MOD(id,2)=1 AND col1 IS NULL THEN student 
+          WHEN MOD(id,2)=0 THEN col2 
+          END AS student
+FROM (SELECT id
+    , student
+    , LEAD(student) OVER() AS col1
+    , LAG(student) OVER() AS col2
+FROM seat) AS subtable
+
+--bài 4
+WITH CTE1 AS (
+SELECT 
+    visited_on
+    ,SUM(amount) AS amount
+FROM customer
+GROUP BY visited_on
+), 
+CTE2 AS (
+SELECT
+    visited_on
+    ,SUM(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as amount
+    ,ROUND(AVG(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) as average_amount
+    ,DENSE_RANK() OVER(ORDER BY visited_on) as rnk
+FROM result
+) 
+SELECT 
+	visited_on
+    ,amount
+    ,average_amount
+FROM CTE2
+WHERE rnk > 6
+
+--baif 5
+
